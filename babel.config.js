c6d41dc19ac8d4f4d321c2ad411b7bcd151a8a1b
Charles Junior <selrahcjunior@hotmail.com>
2022-11-13 22:58:31 -0300

    Arrumando o login que estava com muitos erros

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset',
-  'module:react-native-dotenv'],
-  "plugins": [
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
     ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
       "moduleName": "@env",
-      "path": ".env",
+      "path": ".env"
     }]
   ]
 };
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import { Platform } from 'react-native';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { api } from '../api/api'
+import { env } from 'react-native-dotenv'
 
 interface UserProps {
     name: string;
@@ -12,7 +13,7 @@ interface AuthProviderProps {
     children: ReactNode;
 }
 
-export const CLI_ID_GOOGLE = Platform.OS == 'ios' ? "" : "536961710722-0njap8sm846commv42gh7md2qet3jlj5.apps.googleusercontent.com"
+export const CLI_ID_GOOGLE = Platform.OS == 'ios' ? "" : process.env.CLIENT_ID
 
 
 export interface AuthContextDataProps {
@@ -27,8 +28,18 @@ export function AuthContextProvider({children}: AuthProviderProps){
     const [user, setUser] = useState<UserProps>({} as UserProps);
     const [isUserLoading, setIsUserLoading] = useState(false)
 
+    useEffect(() => {
+        GoogleSignin.configure({
+            webClientId: CLI_ID_GOOGLE,
+        })
+    },[])
+
     const signIn = async () => {
+
         try{
+            const userLogin = await GoogleSignin.signIn();
+
+            
             const userInfo = await GoogleSignin.getTokens();
 
             setIsUserLoading(true);
